01e69d05a2ca716eefb652a5e57981f1acd44257
matu.dev <matu.a.gimenez2002@gmail.com>
2024-06-27 19:52:06 -0300

    prueba de vida

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/test', (req, res) => {
+    res.json({status: 200, message: 'Hello world'})
+})
+
 app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartsRouter)
